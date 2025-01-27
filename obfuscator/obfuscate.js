b189ed5b052a63125d115465312ae4866f8f5d2a
lumecraftor <brentonduenas770@gmail.com>
2025-01-27 17:15:55 +0800

    Update obfuscate.js

--- obfuscator/obfuscate.js
+++ obfuscator/obfuscate.js
@@ -1,1 +1,12 @@
+const crypto = require('crypto');
 
+function obfuscateScript(script) {
+    const encoded = Buffer.from(script).toString('base64');
+    const randomKey = crypto.randomBytes(4).toString('hex');
+    return `-- Obfuscated Script --
+local key = "${randomKey}"
+local obfuscated = "${encoded}"
+loadstring(obfuscated)()`;
+}
+
+module.exports = obfuscateScript;
