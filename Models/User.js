c631544f07f8c47a08be2e6f671f90b81ab553fd
lumecraftor <brentonduenas770@gmail.com>
2025-01-27 17:15:06 +0800

    Update User.js

--- Models/User.js
+++ Models/User.js
@@ -1,1 +1,8 @@
-
+// models/User.js
+const mongoose = require('mongoose');
+const UserSchema = new mongoose.Schema({
+    discordID: { type: String, required: true },
+    key: { type: String, required: true },
+    usageCount: { type: Number, default: 0 },
+});
+module.exports = mongoose.model('User', UserSchema);
